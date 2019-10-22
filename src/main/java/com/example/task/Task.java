59665f784ec90d7e04e95246d1644d0cdd3643c7
wangyy <Administrator@windows10.microdone.cn>
2019-10-22 15:09:50 +0800

    异步执行方法（返回值）

---
+++ src/main/java/com/example/task/Task.java
@@ -0,0 +1,41 @@
+package com.example.task;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+import java.util.concurrent.Future;
+
+@Component
+public class Task {
+
+    @Async
+    public Future<String> doTaskOne() throws Exception {
+        System.out.println("开始做任务一");
+        long start = System.currentTimeMillis();
+        Thread.sleep(1000);
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务一，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务1完成");
+    }
+
+    @Async
+    public Future<String> doTaskTwo()throws Exception {
+        System.out.println("开始做任务二");
+        long start = System.currentTimeMillis();
+        Thread.sleep(1000);
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务二，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务2完成");
+    }
+    @Async
+    public Future<String> doTaskThree() throws Exception {
+        System.out.println("开始做任务三");
+        long start = System.currentTimeMillis();
+        Thread.sleep(1000);
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务三，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务3完成");
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.entity.Sender;
+import com.example.task.Task;
 import com.example.test.TestUser;
 import com.example.test.User;
 import org.junit.Test;
@@ -8,6 +9,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.Future;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,11 +25,32 @@ public class DemoApplicationTests {
 	@Autowired
 	private TestUser testUser;
 
+	@Autowired
+	private Task task;
+
 	@Test
-	public void hello() {
-		System.out.println(testUser.getUserName());
+	public void hello() throws  Exception{
+//		task.doTaskOne();
+//		task.doTaskTwo();
+//		task.doTaskThree();
+		long start = System.currentTimeMillis();
+		Future<String> task1 = task.doTaskOne();
+		Future<String> task2 = task.doTaskTwo();
+		Future<String> task3 = task.doTaskThree();
+//		while(true) {
+//			if(task1.isDone() && task2.isDone() && task3.isDone()) {
+//				System.out.println(task1.get());
+//			// 三个任务都调用完成，退出循环等待
+////				break;
+//			}
+		Thread.sleep(999);
+		if(task1.isDone()) {
+			System.out.println(task1.get());
+		}else
+			System.out.println("task 1 not end");
+//		}
+		long end = System.currentTimeMillis();
+		System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
 	}
-
-
 
 }
